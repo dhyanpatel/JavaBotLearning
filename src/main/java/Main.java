5093ebca4adfc49fb5aea52374ad3a2cc2b75e01
Dhyan <dhyanpp@gmail.com>
2019-01-14 20:41:32 -0600

    Replaced Lambda's with Method References

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.PrivateChannel;
+import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Snowflake;
 import discord4j.voice.json.Ready;
 
@@ -15,13 +17,13 @@ public class Main {
         client.getEventDispatcher().on(ReadyEvent.class)
                 .map(ReadyEvent::getClient)
                 .flatMap(client1 -> client1.getUserById(Snowflake.of(135027504367796224L)))
-                .flatMap(user -> user.getPrivateChannel())
+                .flatMap(User::getPrivateChannel)
                 .flatMap(channel -> channel.createMessage("Bot is up and ready!"))
                 .subscribe();
 
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .map(MessageCreateEvent::getMessage)
-                .flatMap(message -> message.getChannel())
+                .flatMap(Message::getChannel)
                 .flatMap(messageChannel -> messageChannel.createMessage("The User's Original Message"))
                 .subscribe();
 
