8043500bcfcfe7e15aa67de04891a1a1b3e40f71
Dhyan <dhyanpp@gmail.com>
2019-01-14 20:40:00 -0600

    Basic ReadyEvent handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.PrivateChannel;
+import discord4j.core.object.util.Snowflake;
+import discord4j.voice.json.Ready;
 
 public class Main {
     public static void main(String[] args) {
-        DiscordClient client = new DiscordClientBuilder("MzI5NDU0NDczNTk0NDA0ODY2.Dxb_Cw.Zfju6XB3uZvRFsftndQY_ko02oo").build();
-        client.getEventDispatcher().on(MessageCreateEvent.class)
-                .doOnNext(event -> event.getMessage().getContent().ifPresent(c -> System.out.println(c)))
+
+
+        final DiscordClient client = new DiscordClientBuilder("MzI5NDU0NDczNTk0NDA0ODY2.Dxb_Cw.Zfju6XB3uZvRFsftndQY_ko02oo").build();
+
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .map(ReadyEvent::getClient)
+                .flatMap(client1 -> client1.getUserById(Snowflake.of(135027504367796224L)))
+                .flatMap(user -> user.getPrivateChannel())
+                .flatMap(channel -> channel.createMessage("Bot is up and ready!"))
                 .subscribe();
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .flatMap(message -> message.getChannel())
+                .flatMap(messageChannel -> messageChannel.createMessage("The User's Original Message"))
+                .subscribe();
+
+        // Starts up Bot
         client.login().block();
     }
 }
